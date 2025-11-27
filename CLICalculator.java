022474379f75e192f9cc1400818810e995c82039
AbhimanyuPatidar <abhimanyupatidar2511@gmail.com>
2025-11-27 18:16:45 +0530

    Create CLICalculator and implement loop structure

---
+++ CLICalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CLICalculator {
+    public static final String QUIT_CMD = "quit";
+    public static void main(String[] args) {
+        System.out.println("Welcome to Console based calculator!");
+
+        Scanner scan = new Scanner(System.in);
+        String command;
+
+        do {
+            command = scan.nextLine();
+
+        } while (command.equals(QUIT_CMD));
+
+        scan.close();
+    }
+}
